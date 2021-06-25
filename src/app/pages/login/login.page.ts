b5c327b253dd352006156682063f04185e6e772b
Matheus Pacheco <matheus_pachecosilva@hotmail.com>
2021-06-25 00:29:34 -0300

    Iniciando projeto

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonSlides} from "@ionic/angular";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  @ViewChild(IonSlides) slides: IonSlides;
+  public wavesPosition: number = 0;
+  private wavesDifference: number = 100;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  segmentChanged(event: any) {
+    if (event.detail.value === 'login') {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference;
+    } else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+    }
+  }
+
+}
